f8139c7e6bb04162482f52a3cb0725d6ebf210ff
Cameron Smith <smithc11@rpi.edu>
2019-08-05 16:01:43 -0400

    better ar timing parser

--- lassen/allreduceLatency/getTimings.sh
+++ lassen/allreduceLatency/getTimings.sh
@@ -1,6 +1,18 @@
-#!/bin/bash -e
-cat /dev/null > all.csv
-for i in ar_*; do
-  echo $i >> all.csv;
-  grep '^[0-9]' $i | sed -r 's/[[:blank:]]+/,/g' >> all.csv;
+#!/bin/bash -ex
+count=0
+pasteargs=""
+for i in {2..7}; do
+  n=$((2**i))
+  t=$((n*4))
+  f=ar_n${n}*
+  if [ $count = 0 ]; then
+    echo 'size (B)' > sizes
+    grep '^[0-9]' $f | awk '{print $1}' >> sizes
+    pasteargs="$pasteargs sizes"
+  fi
+  echo $n > ${n}
+  grep '^[0-9]' $f | awk '{print $2}' >> ${n}
+  count=$((count+1))
+  pasteargs="$pasteargs $n"
 done
+paste -d "," $pasteargs > all.csv
